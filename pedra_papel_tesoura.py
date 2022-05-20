49bbb5e061a714ae5a7cc323b0bdd4df4646cd5e
owhenrique <paulohenrique272@gmail.com>
2022-05-20 18:13:16 -0300

    Adicionar janela ao jogo

--- pedra_papel_tesoura.py
+++ pedra_papel_tesoura.py
@@ -1,8 +1,11 @@
+import PySimpleGUI as sg
 import random
 
-def jogo(): 
-    usuario = input("Escolha um: pedra 'p', papel 'l' ou tesoura 't'?\n")
-    maquina = random.choice(['p', 'l', 't'])
+# Lógica do jogo
+def jogo(jogada_usuario): 
+    jogada_usuario = jogada_usuario.lower()
+    jogada_maquina = random.choice(['p', 'l', 't'])
+    print(jogada_usuario, jogada_maquina)
     
     # PEDRA > TESOURA, TESOURA > PAPEL, PAPEL > PEDRA
     def vitoria(jogador, oponente):
@@ -11,12 +14,36 @@ def jogo():
             or (jogador == 'l' and oponente == 'p'):
             return True
     
-    if usuario == maquina:
-        return 'Empate :/'
+    if jogada_usuario == jogada_maquina:
+        return 'Empate...'
     
-    if vitoria(usuario, maquina):
+    if vitoria(jogada_usuario, jogada_maquina):
         return 'Você ganhou!!!'
     
-    return 'Você perdeu!'
+    return 'Você perdeu :C'
+
+# Configuração de layout da janela
+sg.theme('Reddit')
+layout = [
+    [sg.Text("Escolha um: Pedra 'P', Papel 'L' ou Tesoura 'T'.")],
+    [sg.Text("Digite a sua escolha:"), sg.Input(key = 'usuario', size=(5,1))],
+    [sg.Button('Jogar'), sg.Button('Sair')],
+]
+
+# Configuração da janela do jogo
+janela = sg.Window('Pedra, papel ou tesoura', layout)
+
+while True:
+    eventos, valores = janela.read()
     
-print(jogo())
+    if eventos == sg.WINDOW_CLOSED:
+        break
+    
+    if eventos == 'Jogar':
+        sg.popup('Resultado da partida', jogo(valores['usuario']))
+    
+    if eventos == 'Sair':
+        break 
+
+    
+janela.close()
